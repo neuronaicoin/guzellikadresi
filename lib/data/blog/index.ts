98a5afd1d85fade3d657bdc2521d3ced7b0b6e74
NEURON AI <neuronaicoin@gmail.com>
2026-06-15 23:14:44 +0300

    Add beylikduzuCiltBakimi post to allPosts

--- lib/data/blog/index.ts
+++ lib/data/blog/index.ts
@@ -7,9 +7,11 @@ import { post as ciltBakimi } from './profesyonel-cilt-bakimi';
 import { post as beylikduzuGuzellik } from './beylikduzu-guzellik-merkezi';
 import { post as beylikduzuLazer } from './beylikduzu-lazer-epilasyon';
 import { post as beylikduzuSacEkimi } from './beylikduzu-sac-ekimi';
+import { post as beylikduzuCiltBakimi } from './beylikduzu-cilt-bakimi';
 
 // Yeni yazı ekleme: import et + bu listeye ekle (en yeni en üstte)
 export const allPosts: BlogPost[] = [
+  beylikduzuCiltBakimi,
   beylikduzuGuzellik,
   beylikduzuLazer,
   beylikduzuSacEkimi,
