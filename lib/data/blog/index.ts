6410835c1f792b2c1c9d4c067692de771caeaa97
NEURON AI <neuronaicoin@gmail.com>
2026-06-16 22:14:03 +0300

    Add bayanKuaforu post to blog index

--- lib/data/blog/index.ts
+++ lib/data/blog/index.ts
@@ -14,9 +14,11 @@ import { post as dovmePiercing } from './dovme-piercing-rehberi';
 import { post as pilates } from './pilates-rehberi';
 import { post as medikalEstetik } from './medikal-estetik-rehberi';
 import { post as erkekKuaforu } from './erkek-kuaforu-rehberi';
+import { post as bayanKuaforu } from './bayan-kuaforu-rehberi';
 
 // Yeni yazı ekleme: import et + bu listeye ekle (en yeni en üstte)
 export const allPosts: BlogPost[] = [
+  bayanKuaforu,
   erkekKuaforu,
   medikalEstetik,
   pilates,
