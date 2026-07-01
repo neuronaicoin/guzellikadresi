2304ceaa2c5d8487ef3f3d9e8bd3930a405e489f
NEURON AI <neuronaicoin@gmail.com>
2026-07-01 22:52:32 +0300

    Add new customer posts for Izmir, Ankara, and Bursa

--- lib/data/blog/index.ts
+++ lib/data/blog/index.ts
@@ -53,10 +53,16 @@ import { post as botoksMuDolgu } from './botoks-mu-dolgu-mu-hangisi-uygun';
 import { post as gmSecerken } from './guzellik-merkezi-secerken-sorulacak-sorular';
 import { post as gelinHazirligi } from './gelin-hazirligi-bakim-takvimi';
 import { post as beylikduzuMusteri } from './beylikduzu-guzellik-bakim-musteri-rehberi';
+import { post as izmirMusteri } from './izmir-guzellik-bakim-musteri-rehberi';
+import { post as ankaraMusteri } from './ankara-guzellik-bakim-musteri-rehberi';
+import { post as bursaMusteri } from './bursa-guzellik-bakim-musteri-rehberi';
 import { post as turkiyePillar } from './turkiye-guzellik-bakim-rehberi-ultimate';
 // Yeni yazı ekleme: import et + bu listeye ekle (en yeni en üstte)
 export const allPosts: BlogPost[] = [
   turkiyePillar,
+  bursaMusteri,
+  ankaraMusteri,
+  izmirMusteri,
   beylikduzuMusteri,
   gelinHazirligi,
   gmSecerken,
