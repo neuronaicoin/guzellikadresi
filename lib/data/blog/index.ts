90c60318cb2cf7fdf371b8cbe0f9fd77ac07d868
NEURON AI <neuronaicoin@gmail.com>
2026-07-01 19:22:20 +0300

    Add Gaziantep beauty post to blog index

--- lib/data/blog/index.ts
+++ lib/data/blog/index.ts
@@ -43,10 +43,12 @@ import { post as ankaraGuzellik } from './ankara-guzellik-merkezi-rehberi';
 import { post as samsunLazer } from './samsun-lazer-epilasyon-rehberi';
 import { post as samsunMedikal } from './samsun-medikal-estetik-rehberi';
 import { post as samsunKuafor } from './samsun-bayan-kuaforu-rehberi';
+import { post as gaziantepGuzellik } from './gaziantep-guzellik-merkezi-rehberi';
 import { post as turkiyePillar } from './turkiye-guzellik-bakim-rehberi-ultimate';
 // Yeni yazı ekleme: import et + bu listeye ekle (en yeni en üstte)
 export const allPosts: BlogPost[] = [
   turkiyePillar,
+  gaziantepGuzellik,
   samsunKuafor,
   samsunMedikal,
   samsunLazer,
