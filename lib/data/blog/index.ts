cd8507e18ad52be3441ea11e1185bfb031daa293
NEURON AI <neuronaicoin@gmail.com>
2026-07-09 19:25:30 +0300

    Add sacEkimiMerkezSecim to allPosts

--- lib/data/blog/index.ts
+++ lib/data/blog/index.ts
@@ -63,8 +63,10 @@ import { post as istanbulMedikal } from './istanbul-medikal-estetik-rehberi';
 import { post as istanbulSacEkimi } from './istanbul-sac-ekimi-rehberi';
 import { post as turkiyePillar } from './turkiye-guzellik-bakim-rehberi-ultimate';
 import { post as lazerMerkezSecim } from './lazer-epilasyon-merkezi-nasil-secilir';
+import { post as sacEkimiMerkezSecim } from './sac-ekimi-merkezi-nasil-secilir';
 // Yeni yazı ekleme: import et + bu listeye ekle (en yeni en üstte)
 export const allPosts: BlogPost[] = [
+  sacEkimiMerkezSecim,
   lazerMerkezSecim,
   turkiyePillar,
   istanbulSacEkimi,
